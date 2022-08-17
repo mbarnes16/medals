8f997519bb709cf93cdf336e0d09b6932eac516a
Maria Barnes <63306174+mbarnes16@users.noreply.github.com>
2022-08-17 12:22:35 -0500

    React Assignment 2 Updated

---
+++ src/components/Country.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Country extends Component {
+  state = { 
+    name: this.props.country.name,
+    gold: this.props.country.gold,
+   }
+   handleIncrement = () => this.setState({ gold: this.state.gold + 1 });
+   handleDecrement = () => this.setState({ gold: this.state.gold - 1 });
+  render() { 
+    return (
+      <div className="country">
+        <div className="name">
+          { this.state.name }
+        </div>
+        <div className="medals">
+          gold medals: { this.state.gold }
+          <button onClick={ this.handleIncrement }>+</button>
+          <button disabled={ this.state.gold === 0} onClick={this.handleDecrement }>-</button>
+        </div>
+        <hr/>
+      </div>
+    );
+  }
+}
+
+export default Country;
